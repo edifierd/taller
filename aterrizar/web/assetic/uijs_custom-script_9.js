bd7f97fb25698ee5afc735993b23b15106dec842
Cosme Fulanito <rodrigo_lp92@hotmail.com>
2017-12-04 17:01:01 -0300

    Vistas

---
+++ aterrizar/web/assetic/uijs_custom-script_9.js
@@ -0,0 +1,29 @@
+/*================================================================================
+	Item Name: Materialize - Material Design Admin Template
+	Version: 4.0
+	Author: PIXINVENT
+	Author URL: https://themeforest.net/user/pixinvent/portfolio
+================================================================================
+
+NOTE:
+------
+PLACE HERE YOUR OWN JS CODES AND IF NEEDED.
+WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR CUSTOM SCRIPT IT'S BETTER LIKE THIS. */
+
+$('.datepicker').pickadate({
+  selectMonths: true, // Creates a dropdown to control month
+  selectYears: 15, // Creates a dropdown of 15 years to control year,
+  today: 'Today',
+  clear: 'Clear',
+  close: 'Ok',
+  closeOnSelect: false // Close upon selecting a date,
+});
+
+$("#opciones input[type='radio']").click(function(){
+    if (this.id == "soloida") {
+      $('#regreso').prop('disabled', true);
+    } else {
+      $('#regreso').prop('disabled', false);
+    }
+    $('#origen').focus();
+});
