172f4994a165a2516b0162cb9e7326cd3dcda104
Cosme Fulanito <rodrigo_lp92@hotmail.com>
2017-12-05 14:36:47 -0300

    JS

--- aterrizar/src/AppBundle/Resources/Public/js/buscador_form.js
+++ aterrizar/src/AppBundle/Resources/Public/js/buscador_form.js
@@ -4,18 +4,27 @@ $( "a" ).click(function() {
     $('#buscar_hotel').hide();
     $('#buscar_auto').hide();
     $('#buscar_vuelo').show();
+    $('#btn_hotel').parent('li').removeClass('active');
+    $('#btn_auto').parent('li').removeClass('active');
+    $('#btn_vuelo').parent('li').addClass('active');
     break;
 
     case 'btn_hotel':
     $('#buscar_vuelo').hide();
     $('#buscar_auto').hide();
     $('#buscar_hotel').show();
+    $('#btn_auto').parent('li').removeClass('active');
+    $('#btn_vuelo').parent('li').removeClass('active');
+    $('#btn_hotel').parent('li').addClass('active');
     break;
 
     case 'btn_auto':
     $('#buscar_hotel').hide();
     $('#buscar_vuelo').hide();
     $('#buscar_auto').show();
+    $('#btn_hotel').parent('li').removeClass('active');
+    $('#btn_vuelo').parent('li').removeClass('active');
+    $('#btn_auto').parent('li').addClass('active');
     break;
 
     default:
